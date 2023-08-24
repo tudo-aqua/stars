d3094900bef394a4d206bbf2746125758769b3ce
Till Schallau <till.schallau@tu-dortmund.de>
2023-08-24 16:54:43 +0200

    Add MetricProviders, first metric implementation and TSCEvaluation

---
+++ stars-core/src/main/kotlin/tools/aqua/stars/core/metrics/ProjectionMetricProvider.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2023 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.core.metrics
+
+import tools.aqua.stars.core.tsc.TSCProjection
+import tools.aqua.stars.core.types.EntityType
+import tools.aqua.stars.core.types.SegmentType
+import tools.aqua.stars.core.types.TickDataType
+
+interface ProjectionMetricProvider<
+    E : EntityType<E, T, S>, T : TickDataType<E, T, S>, S : SegmentType<E, T, S>> :
+    MetricProvider<E, T, S> {
+  fun evaluate(projection: TSCProjection<E, T, S>)
+}
