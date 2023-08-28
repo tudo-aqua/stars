57a60170e1db556ec08e7e0234c6b842c1675a42
Till Schallau <till.schallau@tu-dortmund.de>
2023-08-28 15:21:47 +0200

    Add TotalSegmentTimeLengthMetric with tests

---
+++ stars-core/src/main/kotlin/tools/aqua/stars/core/metric/metrics/TotalSegmentTimeLengthMetric.kt
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2023 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.core.metric.metrics
+
+import tools.aqua.stars.core.metric.providers.SegmentMetricProvider
+import tools.aqua.stars.core.metric.providers.Stateful
+import tools.aqua.stars.core.types.EntityType
+import tools.aqua.stars.core.types.SegmentType
+import tools.aqua.stars.core.types.TickDataType
+
+/**
+ * This class implements the [SegmentMetricProvider] and calculates the total time duration of all
+ * analyzed [SegmentType]s. This metric is [Stateful] and tracks the total time duration.
+ */
+class TotalSegmentTimeLengthMetric<
+    E : EntityType<E, T, S>, T : TickDataType<E, T, S>, S : SegmentType<E, T, S>> :
+    SegmentMetricProvider<E, T, S>, Stateful {
+  /** Holds the current time duration for all already analyzed [SegmentType]s. */
+  private var totalTimeDuration: Double = 0.0
+  /**
+   * Add the duration of the given [segment] to the total duration. [SegmentType] with no, or only
+   * one [TickDataType] are ignores.
+   *
+   * @param segment The [SegmentType] from which the duration should by tracked
+   * @return The current time duration of all analyzed [SegmentType]s
+   *
+   * @throws IllegalStateException When the time duration between the first and last [TickDataType]
+   * of the [SegmentType] is negative.
+   */
+  override fun evaluate(segment: SegmentType<E, T, S>): Double {
+    // The Segment does not have any analyzable time duration
+    if (segment.tickData.isEmpty()) {
+      return totalTimeDuration
+    }
+    // The Segment has only one TickData from which no time duration can be calculated.
+    if (segment.tickData.size == 1) {
+      return totalTimeDuration
+    }
+    // Calculate the time difference between the last and the first tick in the given segment.
+    val segmentTimeDuration =
+        segment.tickData.last().currentTick - segment.tickData.first().currentTick
+    check(segmentTimeDuration >= 0.0) {
+      "The duration between the first and last tick of segment '$segment' should be positive! " +
+          "Actual duration: $segmentTimeDuration."
+    }
+    totalTimeDuration += segmentTimeDuration
+    return totalTimeDuration
+  }
+
+  /**
+   * Returns the current [totalTimeDuration] as [Double].
+   *
+   * @return The current [totalTimeDuration] for all already analyzed [SegmentType]s as [Double].
+   */
+  override fun getState(): Double {
+    return totalTimeDuration
+  }
+
+  /** Prints the current [totalTimeDuration] using [println] */
+  override fun printState() {
+    println("The analyzed segments yielded a total of $totalTimeDuration seconds.")
+  }
+}
