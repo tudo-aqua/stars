e0cdf65b0ef3262612c756c81ca9ed2e10402c18
Till Schallau <till.schallau@tu-dortmund.de>
2023-08-29 19:37:03 +0200

    Add PostEvaluationMetricProvider and add it to the evaluation pipeline

---
+++ stars-core/src/main/kotlin/tools/aqua/stars/core/metric/providers/PostEvaluationMetricProvider.kt
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2023 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.core.metric.providers
+
+import tools.aqua.stars.core.types.EntityType
+import tools.aqua.stars.core.types.SegmentType
+import tools.aqua.stars.core.types.TickDataType
+
+/**
+ * This interface should be implemented by all metrics that have to be called after the evaluation
+ * phase.
+ */
+interface PostEvaluationMetricProvider<
+    E : EntityType<E, T, S>, T : TickDataType<E, T, S>, S : SegmentType<E, T, S>> :
+    MetricProvider<E, T, S> {
+  fun evaluate(): Any?
+  fun print()
+}
