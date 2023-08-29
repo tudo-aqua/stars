d76b78c3b8cb66e7a7b350bc98b80b5e9745249c
Till Schallau <till.schallau@tu-dortmund.de>
2023-08-29 14:36:59 +0200

    Add ValidTSCInstancePerProjectionMetric

---
+++ stars-core/src/main/kotlin/tools/aqua/stars/core/metric/metrics/ValidTSCInstancePerProjectionMetric.kt
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2023 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.core.metric.metrics
+
+import tools.aqua.stars.core.metric.providers.ProjectionAndTSCInstanceNodeMetricProvider
+import tools.aqua.stars.core.metric.providers.Stateful
+import tools.aqua.stars.core.tsc.TSCInstance
+import tools.aqua.stars.core.tsc.TSCInstanceNode
+import tools.aqua.stars.core.tsc.TSCProjection
+import tools.aqua.stars.core.types.EntityType
+import tools.aqua.stars.core.types.SegmentType
+import tools.aqua.stars.core.types.TickDataType
+
+class ValidTSCInstancePerProjectionMetric<
+    E : EntityType<E, T, S>, T : TickDataType<E, T, S>, S : SegmentType<E, T, S>> :
+    ProjectionAndTSCInstanceNodeMetricProvider<E, T, S>, Stateful {
+  /**
+   * Map a [TSCProjection] to a map in which the occurrences of valid [TSCInstanceNode]s are stored:
+   * Map<projection,Map<referenceInstance,List<TSCInstance>>>
+   */
+  val validInstancesMap:
+      MutableMap<
+          TSCProjection<E, T, S>,
+          MutableMap<TSCInstanceNode<E, T, S>, MutableList<TSCInstance<E, T, S>>>> =
+      mutableMapOf()
+
+  override fun evaluate(projection: TSCProjection<E, T, S>, tscInstance: TSCInstance<E, T, S>) {
+    // Check if given tscInstance is valid
+    if (!projection.possibleTSCInstances.contains(tscInstance.rootNode)) {
+      return
+    }
+    validInstancesMap.putIfAbsent(projection, mutableMapOf())
+    val projectionValidInstances = validInstancesMap.getValue(projection)
+    projectionValidInstances.putIfAbsent(tscInstance.rootNode, mutableListOf())
+    // Get already observed instances for current projection
+    val projectionValidInstanceList = projectionValidInstances.getValue(tscInstance.rootNode)
+    // Add current instance to list of observed instances
+    projectionValidInstanceList.add(tscInstance)
+  }
+
+  override fun getState():
+      MutableMap<
+          TSCProjection<E, T, S>,
+          MutableMap<TSCInstanceNode<E, T, S>, MutableList<TSCInstance<E, T, S>>>> {
+    return validInstancesMap
+  }
+
+  override fun printState() {
+    validInstancesMap.forEach { projection, validInstancesMap ->
+      println(
+          "For projection '$projection', there are ${validInstancesMap.size} unique valid instances.")
+      println(validInstancesMap.map { it.value.size })
+    }
+  }
+}
