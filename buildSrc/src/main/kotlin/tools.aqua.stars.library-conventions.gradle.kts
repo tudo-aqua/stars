254b437f0a8480e30e60fbec26b2d8427462311e
Dominik Mäckel <dominik.maeckel@tu-dortmund.de>
2024-07-22 12:36:53 +0200

    Reduced conventions to two files.

--- buildSrc/src/main/kotlin/tools.aqua.stars.library-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/tools.aqua.stars.library-conventions.gradle.kts
@@ -15,11 +15,121 @@
  * limitations under the License.
  */
 
+import org.gradle.accessors.dm.LibrariesForLibs
+import org.gradle.api.publish.plugins.PublishingPlugin.PUBLISH_TASK_GROUP
+import org.gradle.api.tasks.testing.logging.TestLogEvent.*
 import org.gradle.kotlin.dsl.`java-library`
+import tools.aqua.*
+import tools.aqua.GlobalMavenMetadataExtension
+import tools.aqua.MavenMetadataExtension
+import tools.aqua.developer
+import tools.aqua.github
+import tools.aqua.license
 
 plugins {
-  id("tools.aqua.stars.kotlin-conventions")
-  id("tools.aqua.stars.publish-conventions")
+  id("com.dorongold.task-tree")
+  id("com.github.ben-manes.versions")
+  id("com.diffplug.spotless")
+  id("io.gitlab.arturbosch.detekt")
+  id("org.jetbrains.dokka")
 
   `java-library`
+  `maven-publish`
+  signing
+
+  kotlin("jvm")
+}
+
+group = rootProject.group
+
+version = rootProject.version
+
+repositories { mavenCentral() }
+
+tasks.dependencyUpdates {
+  gradleReleaseChannel = "stable"
+  rejectVersionIf(destabilizesVersion)
+}
+
+spotless {
+  kotlinGradle { defaultFormat(rootProject) }
+  // kotlin { defaultFormat(rootProject) }
+}
+
+detekt {
+  basePath = rootProject.projectDir.absolutePath
+  config.setFrom(files(rootProject.file("contrib/detekt-rules.yml")))
+}
+
+val kdocJar: TaskProvider<Jar> by
+    tasks.registering(Jar::class) {
+      archiveClassifier.set("kdoc")
+      from(tasks.dokkaHtml.flatMap { it.outputDirectory })
+    }
+
+val kdoc: Configuration by
+    configurations.creating {
+      isCanBeConsumed = true
+      isCanBeResolved = false
+    }
+
+artifacts { add(kdoc.name, kdocJar) }
+
+val javadocJar: TaskProvider<Jar> by
+    tasks.registering(Jar::class) {
+      archiveClassifier.set("javadoc")
+      from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    }
+
+java {
+  withSourcesJar()
+  withJavadocJar()
+}
+
+// black magic from https://github.com/gradle/gradle/issues/15383
+val libs = the<LibrariesForLibs>()
+
+dependencies {
+  dokkaGfmPlugin(libs.dokka.javadoc)
+
+  testImplementation(platform(libs.junit.bom))
+  testImplementation(libs.bundles.test)
+}
+
+tasks.test {
+  useJUnitPlatform()
+  testLogging { events(FAILED, PASSED, SKIPPED) }
+}
+
+kotlin { jvmToolchain(17) }
+
+val mavenMetadata = extensions.create<MavenMetadataExtension>("mavenMetadata")
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["java"])
+
+      pom {
+        name.set(mavenMetadata.name)
+        description.set(mavenMetadata.description)
+
+        val globalMetadata = rootProject.extensions.getByType<GlobalMavenMetadataExtension>()
+
+        developers { globalMetadata.developers.get().forEach { developer(it.name, it.email) } }
+
+        globalMetadata.githubProject.get().let {
+          github(it.organization, it.project, it.mainBranch)
+        }
+
+        licenses { globalMetadata.licenses.get().forEach { license(it.name, it.url) } }
+      }
+    }
+  }
+}
+
+signing {
+  setRequired { gradle.taskGraph.allTasks.any { it.group == PUBLISH_TASK_GROUP } }
+  useGpgCmd()
+  sign(publishing.publications["maven"])
 }
