ed2dd6d17d6dd3380d78d0b0aed2be4184d9d121
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2024-08-13 16:47:02 +0200

    Fixed hardcoded directories for DataPlotter and DataSaver.

--- stars-core/src/main/kotlin/tools/aqua/stars/core/metric/utils/DataPlotter.kt
+++ stars-core/src/main/kotlin/tools/aqua/stars/core/metric/utils/DataPlotter.kt
@@ -357,5 +357,5 @@ fun <T1 : Number, T2 : Number> getPlot(
  * @return The path to the created folder.
  */
 private fun getAndCreatePlotFolder(folder: String, subFolder: String): String =
-    "analysis-result-logs/${ApplicationStartTimeHolder.applicationStartTimeString}/plots/$folder/$subFolder"
+    "${ApplicationStartTimeHolder.logFolder}/${ApplicationStartTimeHolder.applicationStartTimeString}/plots/$folder/$subFolder"
         .also { File(it).mkdirs() }
