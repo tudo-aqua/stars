f0ca11701d751497e663bee7ca27667832bc3234
Till Schallau <till.schallau@tu-dortmund.de>
2025-01-07 21:28:08 +0100

    Add toolchain resolver

--- buildSrc/settings.gradle.kts
+++ buildSrc/settings.gradle.kts
@@ -18,3 +18,5 @@
 dependencyResolutionManagement {
   versionCatalogs { create("libs") { from(files("../gradle/libs.versions.toml")) } }
 }
+
+plugins { id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0" }
