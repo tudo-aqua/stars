6fdfddbe31dae2cedc0a1c038aa13522f964b7df
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2025-01-08 11:02:58 +0100

    Revert "Removed useless cast."
    This reverts commit f00760b6a6de2be5eea555bb718f11cb8e799792.

--- stars-importer-carla/src/main/kotlin/tools/aqua/stars/importer/carla/carlaToAVConverter.kt
+++ stars-importer-carla/src/main/kotlin/tools/aqua/stars/importer/carla/carlaToAVConverter.kt
@@ -65,11 +65,13 @@ fun convertJsonActorPositionToEntity(
 
   return when (position.actor) {
     is JsonPedestrian ->
-        convertJsonPedestrianToPedestrian(position.actor, tickData, position.positionOnLane, lane)
+        convertJsonPedestrianToPedestrian(
+            position.actor as JsonPedestrian, tickData, position.positionOnLane, lane)
     is JsonTrafficLight -> null
     is JsonTrafficSign -> null
     is JsonVehicle ->
-        convertJsonVehicleToVehicle(position.actor, tickData, position.positionOnLane, lane)
+        convertJsonVehicleToVehicle(
+            position.actor as JsonVehicle, tickData, position.positionOnLane, lane)
   }
 }
 
