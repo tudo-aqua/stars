30f4797f5261be1d877334d44aab9d8814c8b8e5
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2025-10-17 09:50:51 +0200

    Re-added AverageVehiclesInEgoRoadMetric.

---
+++ stars-data-av/src/main/kotlin/tools/aqua/stars/data/av/metrics/AverageVehiclesInEgoRoadMetric.kt
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023-2025 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.data.av.metrics
+
+import java.util.logging.Logger
+import tools.aqua.stars.core.metrics.providers.Loggable
+import tools.aqua.stars.core.metrics.providers.SerializableMetric
+import tools.aqua.stars.core.metrics.providers.Stateful
+import tools.aqua.stars.core.metrics.providers.TickMetricProvider
+import tools.aqua.stars.core.serialization.SerializableIntResult
+import tools.aqua.stars.data.av.dataclasses.*
+
+/**
+ * This class is an implementation of [TickMetricProvider] which provides the metric "average
+ * vehicles in ego's road".
+ *
+ * @property loggerIdentifier identifier (name) for the logger.
+ * @property logger [Logger] instance.
+ */
+class AverageVehiclesInEgoRoadMetric(
+    override val loggerIdentifier: String = "average-vehicles-in-egos-road",
+    override val logger: Logger = Loggable.getLogger(loggerIdentifier),
+) :
+    TickMetricProvider<Actor, TickData, TickDataUnitSeconds, TickDataDifferenceSeconds>,
+    Stateful,
+    SerializableMetric,
+    Loggable {
+
+  private val vehicleCounts = mutableListOf<Int>()
+
+  /**
+   * Evaluates the average count of [Actor]s in the [Road] of the ego vehicle.
+   *
+   * @param tick The [TickData] on which the average count should be calculated.
+   * @return The current count of [Actor]s in the [Road] of the ego vehicle.
+   */
+  override fun evaluate(tick: TickData): Int {
+    val road = tick.ego.lane.road
+
+    val vehicleCount = tick.vehicles.count { it.lane.road == road }
+    vehicleCounts += vehicleCount
+
+    logFiner(
+        "The count of vehicles in Tick '$tick' for ego's road is: ${vehicleCount}. " +
+            "Rolling average is: ${getState()}."
+    )
+
+    return vehicleCount
+  }
+
+  override fun getState(): Double = vehicleCounts.average()
+
+  override fun printState() {
+    logInfo("Average vehicles in ego's road: ${getState()}")
+  }
+
+  override fun getSerializableResults(): List<SerializableIntResult> =
+      vehicleCounts.map {
+        SerializableIntResult(identifier = loggerIdentifier, source = loggerIdentifier, value = it)
+      }
+}
---
+++ stars-data-av/src/test/kotlin/tools/aqua/stars/data/av/metrics/AverageVehiclesInEgosRoadMetricTest.kt
@@ -0,0 +1,107 @@
+/*
+ * Copyright 2023-2025 The STARS Project Authors
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package tools.aqua.stars.data.av.metrics
+
+import kotlin.test.BeforeTest
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import tools.aqua.stars.data.av.dataclasses.Lane
+import tools.aqua.stars.data.av.dataclasses.Road
+import tools.aqua.stars.data.av.dataclasses.TickData
+import tools.aqua.stars.data.av.dataclasses.TickDataUnitSeconds
+import tools.aqua.stars.data.av.dataclasses.Vehicle
+
+/** This class tests the [AverageVehiclesInEgoRoadMetric.evaluate] function. */
+class AverageVehiclesInEgosRoadMetricTest {
+
+  /** [Road] for testing. */
+  lateinit var road: Road
+
+  /** [Lane] for testing. */
+  lateinit var lane: Lane
+
+  /** Test setup. */
+  @BeforeTest
+  fun setup() {
+    lane = Lane()
+    road = Road(lanes = listOf(lane)).also { lane.road = it }
+  }
+
+  /**
+   * This test checks that the returned value of the metric is equal to 1, as there is only the ego
+   * in the block.
+   */
+  @Test
+  fun testOnlyEgoInRoad() {
+    val actor = Vehicle(isEgo = true, lane = lane)
+    val tick = TickData(entities = setOf(actor))
+
+    val averageVehiclesInEgosRoadMetric = AverageVehiclesInEgoRoadMetric()
+    val evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick)
+
+    assertEquals(1, evaluationResult)
+    assertEquals(1.0, averageVehiclesInEgosRoadMetric.getState())
+  }
+
+  /** This test checks that there are (on average) 2 vehicles. */
+  @Test
+  fun testOneVehicleBesidesEgoInRoad() {
+    val ego = Vehicle(id = 0, isEgo = true, lane = lane)
+    val other = Vehicle(id = 1, isEgo = false, lane = lane)
+
+    val tick1 = TickData(currentTickUnit = TickDataUnitSeconds(0.0), entities = setOf(ego, other))
+    val tick2 = TickData(currentTickUnit = TickDataUnitSeconds(1.0), entities = setOf(ego, other))
+    val tick3 = TickData(currentTickUnit = TickDataUnitSeconds(2.0), entities = setOf(ego, other))
+
+    var evaluationResult: Int
+    val averageVehiclesInEgosRoadMetric = AverageVehiclesInEgoRoadMetric()
+
+    evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick1)
+    assertEquals(2, evaluationResult)
+    assertEquals(2.0, averageVehiclesInEgosRoadMetric.getState())
+
+    evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick2)
+    assertEquals(2, evaluationResult)
+    assertEquals(2.0, averageVehiclesInEgosRoadMetric.getState())
+
+    evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick3)
+    assertEquals(2, evaluationResult)
+    assertEquals(2.0, averageVehiclesInEgosRoadMetric.getState())
+  }
+
+  /** This test checks that there are (on average) over 1.5 vehicles. */
+  @Test
+  fun testMostlyOneVehicleBesidesEgoInRoad() {
+    val ego = Vehicle(id = 0, isEgo = true, lane = lane)
+    val other = Vehicle(id = 1, isEgo = false, lane = lane)
+
+    val tick1 = TickData(currentTickUnit = TickDataUnitSeconds(0.0), entities = setOf(ego, other))
+    val tick2 = TickData(currentTickUnit = TickDataUnitSeconds(1.0), entities = setOf(ego))
+
+    var evaluationResult: Int
+    val averageVehiclesInEgosRoadMetric = AverageVehiclesInEgoRoadMetric()
+
+    evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick1)
+    assertEquals(2, evaluationResult)
+    assertEquals(2.0, averageVehiclesInEgosRoadMetric.getState())
+
+    evaluationResult = averageVehiclesInEgosRoadMetric.evaluate(tick2)
+    assertEquals(1, evaluationResult)
+    assertEquals(1.5, averageVehiclesInEgosRoadMetric.getState())
+  }
+}
