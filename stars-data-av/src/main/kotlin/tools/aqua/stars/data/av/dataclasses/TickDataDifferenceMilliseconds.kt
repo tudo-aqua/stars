1a43523741f3ac9232b05d27f878570c113828f1
Dominik Schmid <dominik.schmid@tu-dortmund.de>
2026-09-13 09:50:20 +0200

    Added missing kDoc.

--- stars-data-av/src/main/kotlin/tools/aqua/stars/data/av/dataclasses/TickDataDifferenceMilliseconds.kt
+++ stars-data-av/src/main/kotlin/tools/aqua/stars/data/av/dataclasses/TickDataDifferenceMilliseconds.kt
@@ -26,31 +26,81 @@ import tools.aqua.stars.core.types.TickDifference
  */
 data class TickDataDifferenceMilliseconds(val differenceMillis: Long) :
     TickDifference<TickDataDifferenceMilliseconds>() {
+  /**
+   * Adds a [TickDataDifferenceMilliseconds] to this [TickDataDifferenceMilliseconds].
+   *
+   * @param other The [TickDataDifferenceMilliseconds] to add.
+   * @return A new [TickDataDifferenceMilliseconds] object.
+   */
   override operator fun plus(
       other: TickDataDifferenceMilliseconds
   ): TickDataDifferenceMilliseconds =
       TickDataDifferenceMilliseconds(this.differenceMillis + other.differenceMillis)
 
+  /**
+   * Adds a [Number] considered as milliseconds to this [TickDataDifferenceMilliseconds].
+   *
+   * @param millis The [Number] to add.
+   * @return A new [TickDataDifferenceMilliseconds] object.
+   */
   operator fun plus(millis: Number): TickDataDifferenceMilliseconds =
       TickDataDifferenceMilliseconds(this.differenceMillis + millis.toLong())
 
+  /**
+   * Subtracts a [TickDataDifferenceMilliseconds] from this [TickDataDifferenceMilliseconds].
+   *
+   * @param other The [TickDataDifferenceMilliseconds] to subtract.
+   * @return A new [TickDataDifferenceMilliseconds] object.
+   */
   override operator fun minus(
       other: TickDataDifferenceMilliseconds
   ): TickDataDifferenceMilliseconds =
       TickDataDifferenceMilliseconds(this.differenceMillis - other.differenceMillis)
 
+  /**
+   * Subtracts a [Number] considered as milliseconds from this [TickDataDifferenceMilliseconds].
+   *
+   * @param millis The [Number] to subtract.
+   * @return A new [TickDataDifferenceMilliseconds] object.
+   */
   operator fun minus(millis: Number): TickDataDifferenceMilliseconds =
       TickDataDifferenceMilliseconds(this.differenceMillis - millis.toLong())
 
+  /**
+   * Compares this object with the specified object for order. Returns zero if this object is equal
+   * to the specified [other] object, a negative number if it's less than [other], or a positive
+   * number if it's greater than [other].
+   */
   override operator fun compareTo(other: TickDataDifferenceMilliseconds): Int =
       this.differenceMillis.compareTo(other.differenceMillis)
 
+  /**
+   * Compares this object with the specified [Number] treated as milliseconds for order. Returns
+   * zero if this object is equal to the specified [millis], a negative number if it's less than
+   * [millis], or a positive number if it's greater than [millis].
+   */
   operator fun compareTo(millis: Number): Int = this.differenceMillis.compareTo(millis.toLong())
 
+  /**
+   * Serializes this [TickDataDifferenceMilliseconds] to a [String].
+   *
+   * @return A [String] representation of this [TickDataDifferenceMilliseconds].
+   */
   override fun serialize(): String = this.differenceMillis.toString()
 
+  /**
+   * Deserializes a [String] to a [TickDataDifferenceMilliseconds].
+   *
+   * @param str The [String] to deserialize.
+   * @return A new [TickDataDifferenceMilliseconds] object.
+   */
   override fun deserialize(str: String): TickDataDifferenceMilliseconds =
       TickDataDifferenceMilliseconds(str.toLong())
 
+  /**
+   * Returns a string representation of this [TickDataDifferenceMilliseconds].
+   *
+   * @return A string representation of this [TickDataDifferenceMilliseconds].
+   */
   override fun toString(): String = "${this.differenceMillis}ms"
 }
--- stars-data-av/src/main/kotlin/tools/aqua/stars/data/av/dataclasses/TickDataUnitSeconds.kt
+++ stars-data-av/src/main/kotlin/tools/aqua/stars/data/av/dataclasses/TickDataUnitSeconds.kt
@@ -26,29 +26,85 @@ import tools.aqua.stars.core.types.TickUnit
  */
 data class TickDataUnitSeconds(val tickSeconds: Double) :
     TickUnit<TickDataUnitSeconds, TickDataDifferenceSeconds>() {
+  /**
+   * Adds a [TickDataDifferenceSeconds] to this [TickDataUnitSeconds].
+   *
+   * @param other The [TickDataDifferenceSeconds] to add.
+   * @return A new [TickDataUnitSeconds] object.
+   */
   override operator fun plus(other: TickDataDifferenceSeconds): TickDataUnitSeconds =
       TickDataUnitSeconds(this.tickSeconds + other.differenceSeconds)
 
+  /**
+   * Adds a [Number] considered as seconds to this [TickDataUnitSeconds].
+   *
+   * @param seconds The [Number] to add.
+   * @return A new [TickDataUnitSeconds] object.
+   */
   operator fun plus(seconds: Number): TickDataUnitSeconds =
       TickDataUnitSeconds(this.tickSeconds + seconds.toDouble())
 
+  /**
+   * Subtracts a [TickDataDifferenceSeconds] from this [TickDataUnitSeconds].
+   *
+   * @param other The [TickDataDifferenceSeconds] to subtract.
+   * @return A new [TickDataUnitSeconds] object.
+   */
   override operator fun minus(other: TickDataDifferenceSeconds): TickDataUnitSeconds =
       TickDataUnitSeconds(this.tickSeconds - other.differenceSeconds)
 
+  /**
+   * Subtracts a [TickDataUnitSeconds] from this [TickDataUnitSeconds].
+   *
+   * @param other The [TickDataUnitSeconds] to subtract.
+   * @return A new [TickDataDifferenceSeconds] object.
+   */
   override operator fun minus(other: TickDataUnitSeconds): TickDataDifferenceSeconds =
       TickDataDifferenceSeconds(this.tickSeconds - other.tickSeconds)
 
+  /**
+   * Subtracts a [Number] considered as seconds from this [TickDataUnitSeconds].
+   *
+   * @param seconds The [Number] to subtract.
+   * @return A new [TickDataUnitSeconds] object.
+   */
   operator fun minus(seconds: Number): TickDataUnitSeconds =
       TickDataUnitSeconds(this.tickSeconds - seconds.toDouble())
 
+  /**
+   * Compares this object with the specified object for order. Returns zero if this object is equal
+   * to the specified [other] object, a negative number if it's less than [other], or a positive
+   * number if it's greater than [other].
+   */
   override operator fun compareTo(other: TickDataUnitSeconds): Int =
       this.tickSeconds.compareTo(other.tickSeconds)
 
+  /**
+   * Compares this object with the specified [Number] treated as seconds for order. Returns zero if
+   * this object is equal to the specified [seconds], a negative number if it's less than [seconds],
+   * or a positive number if it's greater than [seconds].
+   */
   operator fun compareTo(seconds: Number): Int = this.tickSeconds.compareTo(seconds.toDouble())
 
+  /**
+   * Serializes this [TickDataUnitSeconds] to a [String].
+   *
+   * @return A [String] representation of this [TickDataUnitSeconds].
+   */
   override fun serialize(): String = this.tickSeconds.toString()
 
+  /**
+   * Deserializes a [String] to a [TickDataUnitSeconds].
+   *
+   * @param str The [String] to deserialize.
+   * @return A new [TickDataUnitSeconds] object.
+   */
   override fun deserialize(str: String): TickDataUnitSeconds = TickDataUnitSeconds(str.toDouble())
 
+  /**
+   * Returns a string representation of this [TickDataUnitSeconds].
+   *
+   * @return A string representation of this [TickDataUnitSeconds].
+   */
   override fun toString(): String = "${this.tickSeconds}s"
 }
